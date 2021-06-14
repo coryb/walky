d09f67e29b955fe2dcac40b10bddc624aeac373c
Cory Bennett <cbennett@netflix.com>
2021-06-14 09:12:11 -0700

    update example to make error handling explicitly ignored

--- walk_test.go
+++ walk_test.go
@@ -384,57 +384,39 @@ someMap:
 bin: baz
 `
 	var root yaml.Node
-	err := yaml.Unmarshal([]byte(doc), &root)
-	if err != nil {
-		panic(err)
-	}
+	_ = yaml.Unmarshal([]byte(doc), &root)
 
 	// we can choose to sort the map if we want
 	sort.Sort(walky.SortableNodeMap(&root))
 
 	// create new *yaml.Node from interface{} value
-	newNode, err := walky.ToNode("new\nvalue")
-	if err != nil {
-		panic(err)
-	}
+	newNode, _ := walky.ToNode("new\nvalue")
 
 	// walk the document tree, looking for keys `someMap`, then
 	// index 0, then key `someKey`.  That node resulting from
 	// the walk is passed to the `func(node *yaml.Node) error`
 	// callback, where we reassign the node.
-	err = walky.WalkPath(&root, func(node *yaml.Node) error {
+	_ = walky.WalkPath(&root, func(node *yaml.Node) error {
 		walky.AssignNode(node, newNode)
 		return nil
 	}, "someMap", 0, "someKey")
-	if err != nil {
-		panic(err)
-	}
 
 	// we can also insert new keys to maps and slices
 	keyNode, _ := walky.ToNode("newkey")
 	newNode, _ = walky.ToNode("new value")
 	// Add comments via code
 	newNode.LineComment = "<-- Look Here"
-	err = walky.AssignMapNode(&root, keyNode, newNode)
-	if err != nil {
-		panic(err)
-	}
+	_ = walky.AssignMapNode(&root, keyNode, newNode)
 
 	// now we will fetch the sequence under someMap and
 	// append a new value to it
 	newNode, _ = walky.ToNode(42)
-	err = walky.WalkPath(&root, func(node *yaml.Node) error {
+	_ = walky.WalkPath(&root, func(node *yaml.Node) error {
 		walky.AppendNode(node, newNode)
 		return nil
 	}, "someMap")
-	if err != nil {
-		panic(err)
-	}
 
-	newDoc, err := yaml.Marshal(&root)
-	if err != nil {
-		panic(err)
-	}
+	newDoc, _ := yaml.Marshal(&root)
 	fmt.Println(string(newDoc))
 	// Output:
 	// # bin is a baz
